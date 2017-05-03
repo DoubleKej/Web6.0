8f957ba9ab504841f321f6bc056b9443ed6b9b33
doublekej <congcuongnguyen.5910@gmail.com>
2017-05-03 16:08:01 +0700

    encrypt Password

--- BE1/modules/api/user/index.js
+++ BE1/modules/api/user/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const Router = express.Router();
 const userController = require('./userController');
-const md5 = require('md5');
 
 Router.post('/', (req, res)=>{
   var userInfo = {
     name: req.body.name,
     userName: req.body.userName,
-    password:req.body.md5(password),
+    password:req.body.password,
     email: req.body.email
   }
   console.log('post data', req.body);
--- BE1/modules/api/user/userModel.js
+++ BE1/modules/api/user/userModel.js
@@ -10,4 +10,30 @@ var userSchema = new Schema({
   email: {type: String}
 });
 
+userSchema.methods = {
+
+    // Authenticate - check if the passwords are the same
+    authenticate: function (password) {
+        return this.encryptPassword(password) === this.hashed_password
+    },
+
+    // Tao salt
+    makeSalt: function () {
+        return Math.round((new Date().valueOf() * Math.random())) + ''
+    },
+
+    // Ma hoa password
+    encryptPassword: function (password) {
+        if (!password) return ''
+            var encrypred
+        try {
+            encrypred = crypto.createHmac('sha1', this.salt).update(password).digest('hex')
+            return encrypred
+        } catch (err) {
+            return ''
+        }
+    }
+}
+
+
 module.exports = mongoose.model('users', userSchema);
